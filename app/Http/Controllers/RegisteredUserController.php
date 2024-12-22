1ca8a2dba1c38d5b2271734384a50a0654b2d825
LisaCee <lisaceedev@gmail.com>
2024-12-21 16:38:43 -0800

    Add php cs fixer. Lint files.

--- app/Http/Controllers/RegisteredUserController.php
+++ app/Http/Controllers/RegisteredUserController.php
@@ -7,27 +7,31 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Validation\Rules\Password;
 
-class RegisteredUserController extends Controller {
+class RegisteredUserController extends Controller
+{
     /**
      * Show the form for creating a new resource.
      */
-    public function create() {
-        return view( 'auth.register' );
+    public function create()
+    {
+        return view('auth.register');
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store( Request $request ) {
-        $userAttributes = $request->validate( [
+    // @todo Add better validation
+    public function store(Request $request)
+    {
+        $userAttributes = $request->validate([
             'name'     => [ 'required' ],
             'email'    => [ 'required', 'email' ],
-            'password' => [ 'required', 'confirmed', Password::min( 6 ) ],
-        ] );
+            'password' => [ 'required', 'confirmed', Password::min(6) ],
+        ]);
 
-        $user = User::create( $userAttributes );
-        Auth::login( $user );
+        $user = User::create($userAttributes);
+        Auth::login($user);
 
-        return redirect( '/' );
+        return redirect('/');
     }
 }
