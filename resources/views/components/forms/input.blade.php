44ff9d64074b817a0835afd4c3c4cd425d41e788
LisaCee <lisaceedev@gmail.com>
2024-12-31 15:08:58 -0800

    Color tweaks.

--- resources/views/components/forms/input.blade.php
+++ resources/views/components/forms/input.blade.php
@@ -5,7 +5,7 @@
         'type' => 'text',
         'id' => $name,
         'name' => $name,
-        'class' => 'rounded-xl bg-white/00 dark:bg-white/10 border border-white/10 px-5 py-4 w-full',
+        'class' => 'rounded-xl bg-white/00 dark:bg-d-surface border border-d-border px-5 py-4 w-full focus:outline-accent-purple',
         'value' => old($name)
     ]
 @endphp
--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -7,8 +7,8 @@
         </div>
     @endif
     <x-page-heading>Profile</x-page-heading>
-    <div class="flex flex-col gap-2 p-5 my-5 border dark:border-d-border rounded-md dark:bg-d-surface">
-        <p>Hi, {{$user->first_name}}</p>
+    <div class="flex flex-col gap-2 p-5 my-5 rounded-md border dark:border-d-border dark:bg-d-surface">
+        <p>Hi, {{$user->first_name}}!</p>
         <p>{{$user->email}}</p>
     </div>
     <div class="flex justify-between">
